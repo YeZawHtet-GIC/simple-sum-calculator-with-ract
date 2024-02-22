eb264cf333157f446879cd9be86e0533b467314d
YeZawHtet-GIC <yezawhtet@gicjp.com>
2024-02-22 11:17:09 +0630

    fix style

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const calculate = (event) => {
     event.preventDefault();
-//!checking provide number
+    //!checking provide number
     if (numberOne === "" || numberTwo === "") {
       alert("Please enter numbers to calculate!");
       return;
@@ -17,7 +17,7 @@ function App() {
     const result = parseInt(numberOne) + parseInt(numberTwo);
     setAdd(result);
   };
-//!preventing input string characters
+  //!preventing input string characters
   const handleNumberChange = (setValue, value) => {
     const regex = /^[0-9\b]+$/; // Regular expression to match numbers
     if (value === "" || regex.test(value)) {
@@ -26,30 +26,32 @@ function App() {
   };
 
   return (
-    <div className="d-flex justify-content-center align-items-center mt-5">
-      <form onSubmit={calculate} className="mt-5 p-5 bg-dark rounded">
-        <label htmlFor="NumberOne">Number One</label>
-        <input
-          type="text"
-          value={numberOne}
-          onChange={(e) => handleNumberChange(setNumberOne, e.target.value)}
-          className="form-control mb-3"
-          placeholder="Enter Number 1"
-        />
-        <label htmlFor="NumberTwo">Number Two</label>
-        <input
-          type="text" 
-          value={numberTwo}
-          onChange={(e) => handleNumberChange(setNumberTwo, e.target.value)}
-          className="form-control mb-3"
-          placeholder="Enter Number 2"
-        />
-        <button type="submit" className="btn btn-primary">
-          Sum
-        </button>
-      </form>
-
-      <h1 className="text-center">Sum Result is : {add}</h1>
+    <div className="bg-dark col-md-6 offset-3 rounded">
+      <h3 className="text-center text-warning pt-4">Adding Two Number</h3>
+      <div className="d-flex justify-content-center align-items-center mt-5">
+        <form onSubmit={calculate} className=" p-2 bg-dark rounded">
+          <label htmlFor="NumberOne">Number One</label>
+          <input
+            type="text"
+            value={numberOne}
+            onChange={(e) => handleNumberChange(setNumberOne, e.target.value)}
+            className="form-control mb-3"
+            placeholder="Enter Number 1"
+          />
+          <label htmlFor="NumberTwo">Number Two</label>
+          <input
+            type="text"
+            value={numberTwo}
+            onChange={(e) => handleNumberChange(setNumberTwo, e.target.value)}
+            className="form-control mb-3"
+            placeholder="Enter Number 2"
+          />
+          <button type="submit" className="btn btn-primary">
+            Sum
+          </button>
+        </form>
+      </div>
+      <h3 className="text-center text-warning py-3">Sum Result is : {add}</h3>
     </div>
   );
 }
